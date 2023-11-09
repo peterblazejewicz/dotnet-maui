4d25fbadfdf32a15e3b140b6911211f292c5feb0
Gerald Versluis <gerald.versluis@microsoft.com>
2023-11-09 12:20:43 +0100

    Fix Android DeviceInfo.Model test for Arm64

--- src/Essentials/test/DeviceTests/Tests/DeviceInfo_Tests.cs
+++ src/Essentials/test/DeviceTests/Tests/DeviceInfo_Tests.cs
@@ -47,6 +47,8 @@ namespace Microsoft.Maui.Essentials.DeviceTests
 					DeviceInfo.Model.Contains("sdk_gphone_x86", StringComparison.Ordinal) ||
 					DeviceInfo.Model.Contains("sdk_gphone64_x86_64", StringComparison.Ordinal) ||
 					DeviceInfo.Model.Contains("sdk_gphone64_arm64", StringComparison.Ordinal) ||
+					DeviceInfo.Model.Contains("sdk_gphone_arm64", StringComparison.Ordinal) ||
+					DeviceInfo.Model.Contains("Android SDK built for arm64", StringComparison.Ordinal) ||
 					DeviceInfo.Model.Contains("google_sdk", StringComparison.Ordinal) ||
 					DeviceInfo.Model.Contains("Emulator", StringComparison.Ordinal) ||
 					DeviceInfo.Model.Contains("Android SDK built for x86", StringComparison.Ordinal);
